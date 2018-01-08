e7fb90af9529a6bd1ef7f2701133eabce6ac651a
tobey <2474398393@qq.com>
2018-01-08 14:39:59 +0800

    多线程，生产者消费者，观察者模式，订阅发布模式总结

---
+++ MultiThread/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadLockDemo_GO();
+
+
+        }
+
+        static void ThreadLockDemo_GO()
+        {
+            ThreadLockDemo c = new ThreadLockDemo();
+
+            Thread t1 = new Thread(c.M1);
+            t1.Start();
+
+            Thread t2 = new Thread(c.M2);
+            t2.Start();
+
+            //注意，这里还不能让 前台线程 结束，因为两个线程可能还没有全部开始
+            Thread.Sleep(1000);
+        }
+    }
+
+    public class ThreadLockDemo
+    {
+        object lock1 = new object();
+
+        object lock2 = new object();
+
+        public void M1()
+        {
+            //（0） M1 正常执行，占用lock1
+            lock (lock1)
+            {
+                Console.WriteLine("M1 Do");
+
+                //Thread.Sleep(100);
+                //（1） 如果执行到这里时 ,t1失去执行权, t2获得执行权,开始执行M2 , 则会进入（2）
+                //（4） 重新获得执行权，继续执行，发现lock2被占用，无法继续 ， 等待M2释放lock2 。。。  // 这时死锁就发生了，（3）（4）都在等待彼此释放资源，
+
+                lock (lock2)
+                {
+                    Console.WriteLine("M1 Do Anthor");
+                }
+            }
+        }
+
+        public void M2()
+        {
+            //（2） M2获得执行权 占用lock2 继续执行 ， 进入（3）
+            lock (lock2)
+            {
+                Console.WriteLine("M2 Do");
+
+                // （3）继续执行时,发现lock1被占用，无法继续， 等待M1释放lock1 。。。   然后执行权重新交给t1,继续执行M1 ， 进入（4）
+                lock (lock1)
+                {
+                    Console.WriteLine("M2 Do Anthor");
+                }
+            }
+        }
+
+    }
+
+}
---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = Subject.GetInstance();
+            var a = new ObserverA(subject);
+            var b = new ObserverB(subject);
+            subject.State = 1;
+            subject.State = 6;
+
+
+            MessageBus.Subscribe("a", new Callback("XD"));
+            MessageBus.Subscribe("a", new Callback("Tobey"));
+            MessageBus.Subscribe("b", new Callback("Ani"));
+            MessageBus.Publish("a", obs =>
+            {
+                (obs as Callback).Do();
+            });
+
+            Console.ReadLine();
+        }
+    }
+
+    #region 观察者模式(一般意义上的)
+
+    public class Subject
+    {
+        private int state;
+
+        private static List<Observer> observers = new List<Observer>();
+
+        private static Subject instance = new Subject();
+
+        private Subject()
+        {
+        }
+
+        public static Subject GetInstance()
+        {
+            return instance;
+        }
+
+        public int State
+        {
+            get
+            {
+                return state;
+            }
+
+            set
+            {
+                state = value;
+                NotifyAllObservers();
+            }
+        }
+
+        public void AddObserver(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void NotifyAllObservers()
+        {
+            foreach (Observer observer in observers)
+            {
+                observer.Update();
+            }
+        }
+    }
+
+    public abstract class Observer
+    {
+        public Subject Subject;
+        public abstract void Update();
+    }
+
+    public class ObserverA : Observer
+    {
+        public ObserverA(Subject subject)
+        {
+            this.Subject = subject;
+            this.Subject.AddObserver(this);
+        }
+
+        public override void Update()
+        {
+            Console.WriteLine("A is ready to update : " + Subject.State);
+        }
+    }
+
+    public class ObserverB : Observer
+    {
+        public ObserverB(Subject subject)
+        {
+            this.Subject = subject;
+            this.Subject.AddObserver(this);
+        }
+
+        public override void Update()
+        {
+            Console.WriteLine("B is ready to update : " + Subject.State);
+        }
+    }
+
+    #endregion
+
+    #region 订阅发布模式（观察者模式另外一种实现方式，两种方式适用场景不同，根据实际情况选择）
+
+    public class MessageBus
+    {
+        private static Dictionary<string, List<object>> dic = new Dictionary<string, List<object>>();
+
+        private static object msgLock = new object();
+
+        public static void Subscribe(string topic, object obj)
+        {
+            if (string.IsNullOrEmpty(topic)) return;
+            lock (msgLock)
+            {
+                if (dic == null)
+                    dic = new Dictionary<string, List<object>>();
+
+                if (!dic.ContainsKey(topic))
+                    dic[topic] = new List<object>();
+
+                dic[topic].Add(obj);
+            }
+        }
+
+        public static void UnSubscribe(object obj)
+        {
+            lock (msgLock)
+            {
+                if (dic == null) return;
+                List<string> removeTopic = new List<string>();
+                foreach (var item in dic)
+                {
+                    item.Value.Remove(obj);
+                    if (item.Value.Count == 0)
+                        removeTopic.Add(item.Key);
+                }
+                foreach (var topic in removeTopic)
+                {
+                    dic.Remove(topic);
+                }
+            }
+        }
+
+        public static void Publish(string topic, Action<object> act)
+        {
+            if (string.IsNullOrEmpty(topic)) return;
+            lock (msgLock)
+            {
+                if (dic == null) return;
+                if (!dic.ContainsKey(topic)) return;
+                var list = dic[topic];
+                foreach (var i in list)
+                {
+                    act(i);
+                }
+            }
+        }
+    }
+    #endregion
+
+
+    public class Callback
+    {
+        public string Name { get; set; }
+
+        public Callback(string name)
+        {
+            Name = name;
+        }
+
+        public void Do()
+        {
+            Console.WriteLine($"my name is {Name}");
+        }
+    }
+}
