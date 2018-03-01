c1477e198ff2792bd142ca538cd12043d420c944
tobey <2474398393@qq.com>
2018-03-01 15:01:12 +0800

    静态代理

---
+++ AOP/DoProxy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOP
+{
+    public class DoProxy : IDo
+    {
+        private IDo _Obj;
+
+        public DoProxy(IDo obj)
+        {
+            _Obj = obj;
+        }
+
+        public void DoSomething()
+        {
+            try
+            {
+                OnBefore();
+                _Obj.DoSomething();
+                OnAfter();
+            }
+            catch (Exception ex)
+            {
+                OnException(ex);
+            }
+        }
+
+
+        public void OnBefore()
+        {
+            Console.WriteLine("OnBefore do");
+        }
+
+        public void OnAfter()
+        {
+            Console.WriteLine("OnAfter do");
+        }
+
+        public void OnException(Exception ex)
+        {
+            Console.WriteLine("发生异常，" + ex.Message);
+        }
+
+    }
+}
---
+++ AOP/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DoProxy proxy = new DoProxy(new Do1());
+            proxy.DoSomething();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Do1 : IDo
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine("Do1 do");
+            throw new Exception("异常测试");
+        }
+    }
+
+}
